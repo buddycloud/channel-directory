913ff6b1806a14e2bf4ed6328824a1aa6309b3de
Abmar Barros <abmargb@gmail.com>
2011-05-31 23:17:17 -0400

    Integrated NearbyQueryHandler to Solr

--- src/com/buddycloud/channeldirectory/handler/nearby/NearbyQueryHandler.java
+++ src/com/buddycloud/channeldirectory/handler/nearby/NearbyQueryHandler.java
@@ -1,15 +1,24 @@
 package com.buddycloud.channeldirectory.handler.nearby;
 
+import java.net.MalformedURLException;
+import java.util.ArrayList;
 import java.util.List;
 import java.util.Properties;
 
+import org.apache.solr.client.solrj.SolrQuery;
+import org.apache.solr.client.solrj.SolrServer;
+import org.apache.solr.client.solrj.SolrServerException;
+import org.apache.solr.client.solrj.impl.CommonsHttpSolrServer;
+import org.apache.solr.client.solrj.response.QueryResponse;
+import org.apache.solr.common.SolrDocument;
+import org.apache.solr.common.SolrDocumentList;
 import org.dom4j.Attribute;
 import org.dom4j.Element;
 import org.xmpp.packet.IQ;
 
 import com.buddycloud.channeldirectory.handler.ChannelQueryHandler;
 import com.buddycloud.channeldirectory.handler.response.ChannelData;
-import com.buddycloud.channeldirectory.handler.response.FakeDataGenerator;
+import com.buddycloud.channeldirectory.handler.response.Geolocation;
 import com.buddycloud.channeldirectory.utils.XMPPUtils;
 
 /**
@@ -20,6 +29,8 @@ import com.buddycloud.channeldirectory.utils.XMPPUtils;
  */
 public class NearbyQueryHandler extends ChannelQueryHandler {
 
+	private static final String SOLR_CHANNELCORE_PROP = "solr.channelcore";
+	
 	public NearbyQueryHandler(Properties properties) {
 		super("http://buddycloud.com/channel_directory/nearby_query", properties);
 	}
@@ -52,13 +63,62 @@ public class NearbyQueryHandler extends ChannelQueryHandler {
 		double lat = Double.valueOf(latAtt.getValue());
 		double lng = Double.valueOf(lngAtt.getValue());
 		
-		List<ChannelData> nearbyObjects = findNearbyObjects(lat, lng);
+		List<ChannelData> nearbyObjects;
+		
+		try {
+			nearbyObjects = findNearbyObjects(lat, lng);
+		} catch (Exception e) {
+			return XMPPUtils.error(iq, "Search could not be performed, service is unavailable.", 
+					getLogger());
+		}
 		
 		return createIQResponse(iq, nearbyObjects);
 	}
 
-	private List<ChannelData> findNearbyObjects(double lat, double lng) {
-		return FakeDataGenerator.createFakeChannels();
+	private List<ChannelData> findNearbyObjects(double lat, double lng) throws MalformedURLException, SolrServerException {
+		SolrServer solrServer = getSolrServer();
+		SolrQuery solrQuery = new SolrQuery("*:*");
+		solrQuery.set("fq", "{!geofilt}");
+		solrQuery.set("sfield", "geoloc");
+		solrQuery.set("pt", lat + "," + lng);
+		solrQuery.set("d", "100");
+		
+		QueryResponse queryResponse = solrServer.query(solrQuery);
+		
+		return convertResponse(queryResponse);
+	}
+	
+	private static List<ChannelData> convertResponse(QueryResponse queryResponse) {
+		List<ChannelData> channels = new ArrayList<ChannelData>();
+		SolrDocumentList results = queryResponse.getResults();
+		
+		for (SolrDocument solrDocument : results) {
+			channels.add(convertDocument(solrDocument));
+		}
+		
+		return channels;
+	}
+
+	private static ChannelData convertDocument(SolrDocument solrDocument) {
+		ChannelData channelData = new ChannelData();
+		String latLonStr = (String) solrDocument.getFieldValue("geoloc");
+		if (latLonStr != null) {
+			String[] latLonSplit = latLonStr.split(",");
+			channelData.setGeolocation(new Geolocation(
+					Double.parseDouble(latLonSplit[0]), 
+					Double.parseDouble(latLonSplit[1])));
+		}
+		
+		channelData.setId((String) solrDocument.getFieldValue("jid"));
+		channelData.setTitle((String) solrDocument.getFieldValue("title"));
+		return channelData;
+	}
+
+	private SolrServer getSolrServer() throws MalformedURLException {
+		String solrChannelUrl = (String) getProperties()
+				.get(SOLR_CHANNELCORE_PROP);
+		SolrServer server = new CommonsHttpSolrServer(solrChannelUrl);
+		return server;
 	}
 
 }
