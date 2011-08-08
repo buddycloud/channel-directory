f3650bb80f429a85eada048a1a34ffb43082c9c7
Abmar <Abmar@Abmar-PC>
2011-08-08 09:36:38 -0300

    Added license headers.

--- src/com/buddycloud/channeldirectory/search/handler/common/mahout/MemoryRecommenderDataModel.java
+++ src/com/buddycloud/channeldirectory/search/handler/common/mahout/MemoryRecommenderDataModel.java
@@ -1,3 +1,18 @@
+/*
+ * Copyright 2011 buddycloud
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
 package com.buddycloud.channeldirectory.search.handler.common.mahout;
 
 import java.io.FileReader;
@@ -21,6 +36,13 @@ import au.com.bytecode.opencsv.CSVReader;
 
 import com.buddycloud.channeldirectory.search.handler.response.ChannelData;
 
+/**
+ * Reads a dump from a CSV file and stores all taste 
+ * data in memory. The CSV file path is addressed by the 
+ * "mahout.dumpfile" property. This is a model for testing purposes
+ * and it is static.
+ * 
+ */
 public class MemoryRecommenderDataModel implements ChannelRecommenderDataModel {
 
 	private Map<String, Long> userToId = new HashMap<String, Long>();
